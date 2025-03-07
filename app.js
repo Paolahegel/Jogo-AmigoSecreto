f65d84a109e816ef625ce36628b6a7d39fc6ff6f
Paola Moura <paolamoura.kj@hotmail.com>
2025-03-07 18:15:04 -0300

    "feat: add desafio resolvido"

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById('amigo');
+    const nomeAmigo = input.value.trim();
+
+    if (nomeAmigo) {
+        amigos.push(nomeAmigo);
+        input.value = '';
+        atualizarListaAmigos();
+    }
+}
+
+function atualizarListaAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    const amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    document.getElementById('apresentacao').textContent = 'Amigo sorteado:';
+    document.getElementById('resultado-amigo').textContent = amigoSorteado;
+}
